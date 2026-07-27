9ea43bf32d18f24359b470b7b20896de0bd5f915
PaoloF16 <paolo.cafis@gmail.com>
2026-07-27 13:37:37 +0200

    start repo

---
+++ src/main/java/PaoloF16/BeCapstoneFinal/BeCapstoneFinalApplication.java
@@ -0,0 +1,13 @@
+package PaoloF16.BeCapstoneFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeCapstoneFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeCapstoneFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PaoloF16/BeCapstoneFinal/BeCapstoneFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package PaoloF16.BeCapstoneFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeCapstoneFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
